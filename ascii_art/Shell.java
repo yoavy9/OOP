528c5eb9915ca1c5bb43a90396ec48c600aeff5e
Yoav Balkany <yoav.balkany@beehero.io>
2024-02-24 12:43:03 +0200

    java for push

---
+++ ascii_art/Shell.java
@@ -0,0 +1,30 @@
+package ascii_art;
+
+public class Shell {
+    public static void main(String[] args) {
+        AsciiOrderLinkedListMap map = new AsciiOrderLinkedListMap();
+
+        // Example usage
+        map.put(0.6, 'c');
+        map.put(0.111, 'a');
+        map.put(0.5, 'b');
+        map.put(0.8, 'e');
+        map.put(0.6, 'd');
+
+        System.out.println("List for key 1:");
+        map.printList(1); // Should print [a, b, c]
+        System.out.println("\n");
+        System.out.println("List for key 2:");
+        map.printList(0.333); // Should print [d, e]
+
+        System.out.println("\n");
+        System.out.println("Max brightness: " + map.getMaxBrightness()); // Should print 2.0
+        System.out.println("Min brightness: " + map.getMinBrightness()); // Should print 1.0
+
+        System.out.println("Character for key 0.22: " + map.getChar(0.22)); // Should print c
+        System.out.println("Character for key 0.8: " + map.getChar(0.8)); // Should print a
+        }
+
+}
+
+
