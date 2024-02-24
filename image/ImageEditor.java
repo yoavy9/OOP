cb87247fe0976fd20bbdd4f55f905b9a7c32cab4
Yoav Balkany <yoav.balkany@beehero.io>
2024-02-24 16:23:20 +0200

    padding

--- image/ImageEditor.java
+++ image/ImageEditor.java
@@ -4,13 +4,19 @@ package image;
  * Defines a contract for image editing operations. Implementations of this interface
  * are expected to provide specific image processing or editing functionality.
  */
-public interface ImageEditing {
-
+public interface ImageEditor {
      /**
       * Executes an image editing operation on the provided Image object.
       *
       * @param image The Image object to be processed.
       * @return The Image object after applying the editing operation.
       */
-     Image execute(Image image);
+     Image paddImage(Image image);
+
+     Image changeColorScheme(Image image);
+
+     Image getSubImage(Image image, int resolution);
+
+
+
 }
