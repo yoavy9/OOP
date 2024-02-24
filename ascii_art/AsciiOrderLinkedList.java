7e4442799ce72236726d67e8a6425a9a84a4b412
Yoav Balkany <yoav.balkany@beehero.io>
2024-02-24 12:53:26 +0200

    deleted output

---
+++ ascii_art/AsciiOrderLinkedList.java
@@ -0,0 +1,45 @@
+package ascii_art;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class AsciiOrderLinkedList {
+    private LinkedList<Character> list;
+
+
+    public AsciiOrderLinkedList() {
+        this.list = new LinkedList<>();
+    }
+
+    // Method to insert a character in ASCII order
+    public void insertInOrder(char value) {
+        ListIterator<Character> iterator = list.listIterator();
+
+        // Find the correct position for insertion
+        while (iterator.hasNext()) {
+            if (iterator.next() > value) {
+                // Previous moves the cursor back to the element just returned by next()
+                iterator.previous();
+                break;
+            }
+        }
+        // Insert the character at the found position
+        iterator.add(value);
+    }
+
+    // todo: remove printer
+    public void printList() {
+        for (Character c : list) {
+            System.out.print(c);
+        }
+    }
+
+    public Character getFirst() {
+        return list.getFirst();
+    }
+
+    // Method to print the list
+    public AsciiOrderLinkedList getAsciiorderList() {
+        return this;
+    }
+}
