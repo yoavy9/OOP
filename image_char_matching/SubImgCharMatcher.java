75ed709fe33e8bc9a2e15b429ee073128257ab64
asafshiloah <asaf.shiloah@mail.huji.ac.il>
2024-02-24 11:57:46 +0200

    Subimg class

---
+++ image_char_matching/SubImgCharMatcher.java
@@ -0,0 +1,24 @@
+package image_char_matching;
+
+public class SubImgCharMatcher {
+
+    private AsciiHshmap asciiHshmap;
+    private final char[] charset;
+
+    public SubImgCharMatcher(char[] charset){ //hashmap
+        this.charset = charset;
+    }
+
+    public char getCharByImageBrightness(double brightness){
+        return asciiHshmap.getChar(brightness);
+    }
+
+    public void addChar(char c){
+        asciiHshmap.addChar(c);
+    }
+
+    public void removeChar(char c){
+        asciiHshmap.removeChar(c);
+    }
+
+}
